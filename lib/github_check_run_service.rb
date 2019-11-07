eea14e9544fa4600875d9c8709fe36b45b41f417
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-07 14:50:14 -0500

    remove stray puts

--- lib/github_check_run_service.rb
+++ lib/github_check_run_service.rb
@@ -19,8 +19,6 @@ class GithubCheckRunService
     @annotations = @report_adapter.annotations(@report)
     @conclusion = @report_adapter.conclusion(@report)
 
-    puts update_check_payload
-
     @client.patch(
       "#{endpoint_url}/#{id}",
       update_check_payload

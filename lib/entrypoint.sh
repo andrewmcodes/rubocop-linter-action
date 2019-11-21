045ba0ddda56c409040ecf5b93e0cbc2f4603108
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-21 17:46:59 -0500

    format

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,18 +2,15 @@
 
 set -e
 
-if [ "${INPUT_BUNDLE}" = "true" ]
-then
+if [ "${INPUT_BUNDLE}" = "true" ]; then
   bundle install --deployment --jobs 4 --retry 3
-elif [ -z "${INPUT_VERSION}" ]
-then
+elif [ -z "${INPUT_VERSION}" ]; then
   gem install rubocop
 else
   gem install rubocop -v "${INPUT_VERSION}"
 fi
 
-if [ -n "${INPUT_ADDITIONAL_GEMS}" ]
-then
+if [ -n "${INPUT_ADDITIONAL_GEMS}" ]; then
   gem install "${INPUT_ADDITIONAL_GEMS}"
 fi
 
